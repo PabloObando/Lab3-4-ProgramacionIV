80e9fe9b05dbff3059b0035f609dfd8373504e23
PabloObando <pabloobando03@gmail.com>
2025-04-02 21:06:11 -0600

    commit

---
+++ src/Components/AnonymusUserComponent.jsx
@@ -0,0 +1,16 @@
+export default function AnonymusUserComponent(){
+
+    return (
+        <>
+    
+               <h2>Hola, por favor accede al sitio para obtener mayor informacion</h2>
+            
+                
+            
+    
+                    
+        </>
+      )
+    }
+    
+    
---
+++ src/Components/WelcomeWidget.jsx
@@ -0,0 +1,7 @@
+
+export default function WelcomeWidget(props){
+
+    return(
+        <h2>Welcome to our site (props.email)</h2>
+    )
+}
